6a38c4aff246b13cc27e3c618bdf833ff66f0e39
Rajat Bisoi <rajatbisoi1@gmail.com>
2021-07-22 13:45:40 +0530

    added logger class

---
+++ logger/logit.py
@@ -0,0 +1,31 @@
+import concurrent.futures 
+import logging
+from datetime import datetime
+
+
+class Logit:
+
+    """
+    logger class
+    use this class to log the execution of the program.
+
+    code for usage:
+    
+    from logger.logit import Logit
+    l = logit()
+    l.log("message",type=logging.error)
+         
+    """
+    def __init__(self):
+        logging.basicConfig(filename='execution.log', level=logging.DEBUG)
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1) 
+
+    def log(self, msg, type=logging.info):
+        self.executor.submit(type, f"{datetime.now().date()} {datetime.now().strftime('%H:%M:%S')} {msg}")
+
+
+# if __name__=="__main__":
+#     l = Logit()
+#     for i in range(10):
+#         l.log("I'm a log",type=logging.error)
+#     l.log("test")
