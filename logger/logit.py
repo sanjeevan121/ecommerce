d4d1ddc2ea4bc28bc3e728d626fde999d4c867bc
Rajat Bisoi <rajatbisoi1@gmail.com>
2021-07-30 21:47:39 +0530

    logger updated for mongodb

--- logger/logit.py
+++ logger/logit.py
@@ -1,8 +1,8 @@
 import concurrent.futures 
 import logging
 from datetime import datetime
-
-
+import pymongo as pmg
+import uuid
 class Logit:
 
     """
@@ -11,17 +11,31 @@ class Logit:
 
     code for usage:
     
-    from logger.logit import Logit
-    l = logit()
-    l.log("message",type=logging.error)
+    >>>from logger.logit import Logit
+    >>>l = Logit()
+    >>>l.log("scope","message")
          
     """
     def __init__(self):
         logging.basicConfig(filename='execution.log', level=logging.DEBUG)
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1) 
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
-    def log(self, msg, type=logging.info):
-        self.executor.submit(type, f"{datetime.now().date()} {datetime.now().strftime('%H:%M:%S')} {msg}")
+        DEFAULT_CONNECTION_URL = 'localhost:27017'
+        client = pmg.MongoClient(DEFAULT_CONNECTION_URL)
+
+        self.conn = client["execution_log"]["log"] 
+        # conn.insert_one()
+    def log(self, scope,msg):
+
+        id_obj=self.conn.find({}, {"_id"})
+        idxt = []
+        for idx in id_obj:
+            idxt.append(idx)
+
+        if datetime.now().date() in idxt:
+            self.executor.submit(self.conn.update, {"_id":datetime.now().date(),f"{uuid.uuid1()}":f"{datetime.now().date()} {datetime.now().strftime('%H:%M:%S')} {scope} {msg}"})
+        else:
+            self.executor.submit(self.conn.insert_one, {"_id":datetime.now().date(),f"{uuid.uuid1()}":f"{datetime.now().date()} {datetime.now().strftime('%H:%M:%S')} {scope} {msg}"})
 
 
 # if __name__=="__main__":
